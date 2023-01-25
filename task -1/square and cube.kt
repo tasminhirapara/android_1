1ec966324437a88e8ed9565de21b069247e1a87a
tasmin hirapara <112859079+tasminhirapara@users.noreply.github.com>
2023-01-25 17:15:46 +0530

    Add files via upload

---
+++ task -1/square and cube.kt
@@ -0,0 +1,9 @@
+fun main()
+{
+    println("enter your number")
+    var a= readLine()!!.toFloat()
+    var b=a*a*a
+    var c=a*a
+    println("your number square : $c")
+    println("your number cube  : $b")
+}
---
+++ task -1/triangle.kt
@@ -0,0 +1,9 @@
+fun main()
+{
+    print("enter value of a :")
+    var a= readLine()!!.toFloat()
+    print("enter value of b :")
+    var b= readLine()!!.toFloat()
+    var area =(a*b)/2
+    println("area of triangle is :  $area")
+}
