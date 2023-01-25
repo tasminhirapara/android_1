b5c2f83c1c67b3660bcd5678533822c7461e6bbb
tasmin hirapara <112859079+tasminhirapara@users.noreply.github.com>
2023-01-25 17:13:58 +0530

    Add files via upload

---
+++ task -1/addition.kt
@@ -0,0 +1,18 @@
+fun main()
+{
+    var a=6
+    var b=5
+    var c=a+b
+    var d=a-b
+    var e=a/b
+    var f=a*b
+
+
+    println("addition is : $c")
+    println("subtraction is : $d")
+    println("division is : $e")
+    println("multiplication is : $f")
+
+
+
+}
