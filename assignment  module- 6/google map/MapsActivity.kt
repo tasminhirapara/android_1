876a3b004761af7212f0fecb428d81ce43f11a76
tasmin hirapara <112859079+tasminhirapara@users.noreply.github.com>
2023-07-15 13:48:50 +0530

    Add files via upload

--- assignment  module- 6/google map/MapsActivity.kt
+++ assignment  module- 6/google map/MapsActivity.kt
@@ -2,6 +2,7 @@ package com.example.googlemaps
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.location.Geocoder
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
@@ -9,6 +10,8 @@ import android.location.LocationManager.GPS_PROVIDER
 import android.location.LocationManager.NETWORK_PROVIDER
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 
@@ -23,113 +26,32 @@ import java.util.Objects as Objects1
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
 
-    private lateinit var binding: ActivityMapsBinding
-    private lateinit var mapView: MapView
-    private lateinit var googleMap: GoogleMap
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
-
-    var lat = 0.00
-    var lng = 0.00
+    private lateinit var map: GoogleMap
+    private lateinit var addressEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMapsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-//
-        mapView = findViewById(R.id.mapView)
-        mapView.onCreate(savedInstanceState)
-        mapView.getMapAsync(this)
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-//
-  }
+        setContentView(R.layout.activity_maps)
 
+        addressEditText = findViewById(R.id.addressEditText)
+        val displayMarkerButton: Button = findViewById(R.id.displayMarkerButton)
 
-    override fun onMapReady(map: GoogleMap) {
-        googleMap = map
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION
-                ),
-                PERMISSIONS_REQUEST_LOCATION
-            )
-            return
-        }
-        googleMap.isMyLocationEnabled = true
-        showCurrentLocation()
-    }
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
-    private fun showCurrentLocation() {
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
-        }
-        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
-            location?.let {
-                val currentLatLng = LatLng(location.latitude, location.longitude)
-                googleMap.addMarker(MarkerOptions().position(currentLatLng).title("Current Location"))
-                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, DEFAULT_ZOOM))
+        displayMarkerButton.setOnClickListener {
+            val address = addressEditText.text.toString()
+            val geocoder = Geocoder(this)
+            val addresses = geocoder.getFromLocationName(address, 1)
+            if (addresses!!.isNotEmpty()) {
+                val location = LatLng(addresses[0].latitude, addresses[0].longitude)
+                map.addMarker(MarkerOptions().position(location))
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 15f))
             }
         }
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == PERMISSIONS_REQUEST_LOCATION) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                onMapReady(googleMap)
-            }
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        mapView.onResume()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        mapView.onPause()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mapView.onDestroy()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        mapView.onSaveInstanceState(outState)
-    }
-
-    companion object {
-        private const val DEFAULT_ZOOM = 15f
-        private const val PERMISSIONS_REQUEST_LOCATION = 100
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
     }
 }
